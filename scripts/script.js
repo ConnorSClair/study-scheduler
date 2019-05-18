cf2128d956d9430c515ca5f8523f82e8f22b78f2
Connor Macleod <connormusique@gmail.com>
2019-05-18 17:29:18 +1000

    adding buttons. Need to make functions to avoid duplicate code around button creation and coloring

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,38 @@
 // temporary - remove data on load
 localStorage.removeItem("test")
+const topicNames = "topic-names"
+
+function load() {
+    var topicNameData = localStorage.length;
+    if (topicNameData == 0) {
+        // first time page is loaded
+    } else {
+        // create a button for each topic in topicNameData
+        for (i = 0; i < localStorage.length; i++) {
+            var button = document.createElement("BUTTON");
+            button.innerHTML = localStorage.key(i);
+            document.body.appendChild(button);
+        }
+    }
+}
+// query database for study topics 
+// create buttons dynamically (names and colors)
+
 
 function submitTopic() {
     var name = document.getElementById("new-topic").value
     if (name == "") {
-        alert("Couldn't see any text")
-        return 
+        alert("Couldn't see any text");
+        return;
     }
     // check if button with that name already exists, if not:
     if (localStorage.getItem(name) == null) {
         // create new button
         var dates_data = JSON.stringify([]);
         localStorage.setItem(name,dates_data);
+        var button = document.createElement("BUTTON")
+        button.innerHTML = name
+        document.body.appendChild(button)
     } else {
         // else alert user 
         alert("Topic with that name already exists")
@@ -24,21 +45,24 @@ function studied() {
     var dates = JSON.parse(localStorage.getItem(name));
     debugger;
     // assume not null
-    for (let dateStr of dates) {
-        // convert date string to Date and check if already studied today
+    dates.forEach(dateStr => {
         date = new Date(dateStr)
         if (sameDay(today,date)) {
             return
         }
-    }
+    });
     dates.push(today);
-    localStorage.setItem(name, JSON.stringify(dates))
+    document.getElementById("test-topic").style.backgroundColor = "green";
+    localStorage.setItem(name, JSON.stringify(dates));
 }
+
+
+
 
 function getStudyDates() {
     var name = "test";
     var dates = JSON.parse(localStorage.getItem(name));
-    for (let item of dates) console.log(item);
+    dates.forEach(dateStr => console.log(dateStr));
 }
 
 function sameDay(d1, d2) {
@@ -46,3 +70,8 @@ function sameDay(d1, d2) {
       d1.getMonth() == d2.getMonth() &&
       d1.getDate() == d2.getDate();
   }
+function print_local_storage() {
+    for ( var i = 0, len = localStorage.length; i < len; ++i ) {
+    console.log( localStorage.getItem( localStorage.key( i ) ) );
+  }
+}
