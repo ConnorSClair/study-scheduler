56c4c1e78222cc448303d82b7abef3a76c453d86
ConnorSClair <connor.s.clair@gmail.com>
2019-05-21 19:43:31 +1000

    Improved look of webpage. Made some bug fixes. Refactored some code. Still WIP.

--- scripts/script.js
+++ scripts/script.js
@@ -15,6 +15,7 @@ function create_button(name) {
     button.id = name;
     document.getElementById("buttons").appendChild(button);
     document.getElementById(name).setAttribute("onclick","studied(this)");
+    document.getElementById(name).setAttribute("class","waves-effect waves-light btn-large");
 }
 
 /* given a button that exists, colorise based on streak logic
@@ -22,18 +23,17 @@ function create_button(name) {
 function colorise_button(name) {
     // simple first. studied today then green, otherwise grey
     if (name != "") {
-        dates = retrieveData(name)
-        if (dates.length == 0) {
+        var dates = retrieveData(name)
+        if (dates.length === 0) {
             document.getElementById(name).style.backgroundColor = "grey";
             return;
         }
         // assumes ordered from smallest to largest (latest date is at end of list)
-        date = Date(dates[dates.length - 1]) 
-        if (sameDay(new Date(date),new Date())) {
-            document.getElementById(name).style.backgroundColor = "green";
-        } else {
-            document.getElementById(name).style.backgroundColor = "grey";
-        }        
+        const latestDate = new Date(dates[dates.length - 1]) 
+        const nameElement = document.getElementById(name);
+        nameElement.style.backgroundColor= sameDay(latestDate, new Date()) ?
+            '#0c875a' :
+            '#286aa8';
     }
 }
 
@@ -55,11 +55,11 @@ appropriately handles no text or duplicate topics
 */ 
 function newTopic() {
     var topic = document.getElementById("new-topic").value
-    if (topic == "") {
+    if (topic === "") {
         return;
     }
     // check if button with that name already exists
-    if (retrieveData(topic) == null) {
+    if (retrieveData(topic) === null) {
         storeData(topic,[]);
         create_button(topic);
         colorise_button(topic);
@@ -88,9 +88,9 @@ function studied(button) {
 }
 
 function sameDay(d1, d2) {
-    return d1.getFullYear() == d2.getFullYear() &&
-      d1.getMonth() == d2.getMonth() &&
-      d1.getDate() == d2.getDate();
+    return d1.getFullYear() === d2.getFullYear() &&
+      d1.getMonth() === d2.getMonth() &&
+      d1.getDate() === d2.getDate();
 }
 
 function deleteTopic() {
@@ -106,110 +106,3 @@ function deleteAllData() {
         localStorage.removeItem(localStorage.key(i));
     }
 }
-
-/*
-function submitTopicB() {
-    var name = document.getElementById("new-topic").value
-    if (name == "") {
-        alert("Couldn't see any text");
-        return;
-    }
-    // check if button with that name already exists, if not:
-    if (localStorage.getItem(name) == null) {
-        // create new button
-        var dates_data = JSON.stringify([]);
-        localStorage.setItem(name,dates_data);
-        var button = document.createElement("BUTTON")
-        button.innerHTML = name
-        button.id = name;
-        document.body.appendChild(button)
-        document.getElementById(name).setAttribute("onclick","studied(this)");
-    } else {
-        // else alert user 
-        alert("Topic with that name already exists")
-    }
-}
-
-// temporary - remove data on load
-// removeAll();
-
-function load() {
-    var topicNameData = localStorage.length;
-    if (topicNameData == 0) {
-        // first time page is loaded
-    } else {
-        // create a button for each topic in topicNameData
-        for (i = 0; i < localStorage.length; i++) {
-            var button = document.createElement("BUTTON");
-            name = localStorage.key(i)
-            button.innerHTML = name;
-            button.id = name;
-            //button.onclick = alert("hey");
-            document.body.appendChild(button);
-            document.getElementById(name).setAttribute("onclick","studied(this)");
-            document.getElementById(name).style.backgroundColor = "grey"
-        }
-    }
-    colorise();
-}
-
-
-function colorise() {
-    for (i = 0; i < localStorage.length; i++) {
-        var today =  new Date();
-        var name = localStorage.key(i);
-        var dates = JSON.parse(localStorage.getItem(name));
-        if (dates != null) {
-            for (var dateStr of dates) {
-                date = new Date(dateStr);
-                if (sameDay(today,date)) {
-                    document.getElementById(name).style.backgroundColor = "blue"
-                    break;
-                }
-            }
-        }
-    }
-}
-// query database for study topics 
-// create buttons dynamically (names and colors)
-
-
-function studied(button) {
-    var name = button.id
-    const today = new Date();
-    var dates = JSON.parse(localStorage.getItem(name));
-    // assume not null
-    if (dates != null) {
-        for (var dateStr of dates) {
-            date = new Date(dateStr)
-            if (sameDay(today,date)) {
-                return
-            }
-        }
-    }
-    dates.push(today);
-    document.getElementById(name).style.backgroundColor = "green";
-    localStorage.setItem(name, JSON.stringify(dates));
-}
-
-
-
-
-
-
-
-
-
-//useless functions below
-function getStudyDates() {
-    return 0
-    var dates = JSON.parse(localStorage.getItem(name));
-    dates.forEach(dateStr => console.log(dateStr));
-}
-function print_local_storage() {
-    for ( var i = 0, len = localStorage.length; i < len; ++i ) {
-    console.log( localStorage.getItem( localStorage.key( i ) ) );
-  }
-}
-
-*/
